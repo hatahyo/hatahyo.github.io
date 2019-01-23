d7c64569aaa815e2344a441022ee7e4fadb180a2
Hyoma Hata <hyoma.hata@techtouch.co.jp>
2019-01-23 18:55:57 +0900

    nuv

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "Hyoma's Story ",
+    description: 'Just playing around',
+
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Git', link: '/git/' },
+        ]
+      }
+  }
